1e11d12920b6ab1ee58fb688c257ec798c55203f
Flasushi <flash.osushi@gmail.com>
2018-07-05 00:12:34 +0900

    外部jsにした

--- Acceleration.js
+++ Acceleration.js
@@ -1,26 +1,21 @@
-window.addEventListener('devicemotion', deviceMotionHandler);
+      // 加速度イベント
+      window.addEventListener("devicemotion", accelerationEvent);
 
-function deviceMotionHandler(event){
+      // イベント発生
+      function accelerationEvent(event) {
+          // x軸
+          var x = event.acceleration.x;
+          // y軸
+          var y = event.acceleration.y;
+          // z軸
+          var z = event.acceleration.z;
 
-  //取得
+                  // 取得した値を表示
+          xDir = document.getElementById("xDir");
+          yDir = document.getElementById("yDir");
+          zDir = document.getElementById("zDir");
+          xDir.innerHTML = 'x=' + x;
+          yDir.innerHTML = 'y=' + y;
+          zDir.innerHTML = 'z=' + z;
 
-  const x = event.acceleration.x;
-
-  const y = event.acceleration.y;
-
-  const z = event.acceleration.z;
-
-  //値を表示
-
-  xDir = document.getElementByid("xDir");
-
-  yDir = document.getElementByid("yDir");
-
-  zDir = document.getElementByid("zDir");
-
-  xDir.innerHTML = 'x=' + x;
-
-  yDir.innerHTML = 'y=' + y;
-
-  zDir.innerHTML = 'z=' + z;
-}
+      }
