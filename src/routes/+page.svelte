09a457c66c0fed4d8c78134a6c54e18366870410
Flasushi <flash.osushi@gmail.com>
2024-12-04 22:41:24 +0900

    fix

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    let src = "/icon.jpg";
+</script>
+
+<style>
+    ur, li {
+        background-color: lightblue;
+    }
+    li {
+        display: block;
+        border: dotted 1px black;
+        padding-left: 1em;
+        padding-right: 1em;
+        width: 10em;
+    }
+</style>
+
+<h3>Hi! My name is Flasushi</h3>
+<div>
+    <img src={src} alt="flasushi" />
+</div>
+<br>
+<ur>
+    <li>computer science</li>
+    <li>neurodevelopmental</li>
+    <li>psychology</li>
+</ur>
